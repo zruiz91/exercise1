7ab5820f512640a12d87bef4e6950ddda59cad64
Zach Ruiz <powersbooth10@gmail.com>
2023-06-13 14:31:33 -0500

    refactored solution code.

--- exercise1.py
+++ exercise1.py
@@ -8,4 +8,9 @@ two_digit_number = input("Type a two digit number: ")
 
 ####################################
 #Write your code below this line 👇
-print(int(two_digit_number[0]) + int(two_digit_number[1]))
+
+first_digit =int(two_digit_number[0])
+
+second_digit =int(two_digit_number[1])
+
+print(first_digit + second_digit)
